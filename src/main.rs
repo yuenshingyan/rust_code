636791e77b78548f7d6c935777fa0d21d5139768
Hindy <yuenshingyan@gmail.com>
2023-09-23 21:10:17 +0800

    add examples

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,66 @@
-use serde::{Deserialize};
 use reqwest;
+use serde_json::Value;
 use polars::prelude::*;
 
-#[derive(Debug, Deserialize)]
-struct Data {
-    name: String,
-    url: String,
-    #[serde(rename = "sport-id")]
-    sport_id: u32,
-    #[serde(rename = "sport-url")]
-    sport_url: String,
-    #[serde(rename = "tournament-id")]
-    tournament_id: u32,
-}
-
-#[derive(Debug, Deserialize)]
-struct D {
-    data: Vec<Data>,
-    title: String,
-    section_link: String,
-    section_link_name: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Response {
-    s: u32,
-    d: D,
-    refresh: u32,
-}
-
-// {
-//     "s": 1,
-//     "d": {
-//         "data": [
-//             {
-//                 "name": "Premier League",
-//                 "url": "\/football\/england\/premier-league\/",
-//                 "sport-id": 1,
-//                 "sport-url": "football",
-//                 "tournament-id": 1
-//             },
-//         ],
-//         "title": "Top Events",
-//         "section_link": "\/events\/",
-//         "section_link_name": "All events"
-//     },
-//     "refresh": 20
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
 // }
+
+fn get_actual_value(value: Value) -> Option<String> {
+    match value {
+        Value::String(string_value) => Some(string_value),
+        Value::Number(number_value) => Some(number_value.to_string()),
+        Value::Bool(bool_value) => Some(bool_value.to_string()),
+        Value::Null => Some("null".to_string()),
+        _ => None,
+    }
+}
 
 // tokio let's us use "async" on our main function
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert("authority", "www.oddsportal.com".parse().unwrap());
+    headers.insert("accept", "application/json, text/plain, */*".parse().unwrap());
+    headers.insert("accept-language", "en-US,en-GB;q=0.9,en;q=0.8".parse().unwrap());
+    headers.insert("content-type", "application/json".parse().unwrap());
+    headers.insert(reqwest::header::COOKIE, "op_cookie-test=ok; op_user_time=1694833059; op_user_cookie=5934834206; op_user_hash=9a17e2fc8d0b65105daae26aee2e47d1; _ga=GA1.1.2015866974.1694833061; op_user_time_zone=8; op_user_full_time_zone=75; OptanonAlertBoxClosed=2023-09-16T02:57:43.336Z; _sg_b_n=1694833063372; _hjSessionUser_3147261=eyJpZCI6ImU4ZjRiYzQwLWE4MTMtNTEzMC05Yjg0LTZhN2RhZjgzMDlkYSIsImNyZWF0ZWQiOjE2OTQ4MzMwNjA2MzMsImV4aXN0aW5nIjp0cnVlfQ==; eupubconsent-v2=CPyLPsAPyLPsAAcABBENDYCsAP_AAAAAAChQJGtf_X__b2_j-_7-f_t0eY1P9_7_v-0zjhfdF-8N2f_X_L8X52M5vF16pqoKuR4ku3bBIQVlHOHcDUmw6okVryPsbk2cr7NKJ7PkmlMbM2dYGH9_n93T-ZKY7___f__z_v-v___9____7-3f3__p__--2_e_V_89zfn9_____9vP___9v-_9_3gAAAAAAAAAAAAD4AAABQkAIAGgC8x0AQAGgAZgBlALzIQAQBlEoAIC8ykAQAGgAZgBlALz.f_gAAAAAAAAA; _hjIncludedInSessionSample_3147261=0; _hjSession_3147261=eyJpZCI6IjE4NjUyN2JkLWI1MTEtNDkzOC1hZTA3LTY1ZGY0ZTc5YmRjMSIsImNyZWF0ZWQiOjE2OTU0NTg2NjM5MTIsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=1; OptanonConsent=isGpcEnabled=0&datestamp=Sat+Sep+23+2023+16%3A44%3A23+GMT%2B0800+(Singapore+Standard+Time)&version=202210.1.0&isIABGlobal=false&hosts=&consentId=93886366-8b53-4b16-9566-9951a695145f&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0004%3A1%2CSTACK42%3A1&geolocation=SG%3B&AwaitingReconsent=false; _sg_b_p=%2Ffootball%2Fengland%2Fpremier-league-2022-2023%2Fresults%2F; _sg_b_v=5%3B39650%3B1695458664; _ga_5YY4JY41P1=GS1.1.1695458663.6.1.1695458827.60.0.0".parse().unwrap());
+    headers.insert("referer", "https://www.oddsportal.com/football/england/premier-league-2021-2022/results/".parse().unwrap());
     headers.insert("sec-ch-ua", "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"".parse().unwrap());
-    headers.insert("X-XSRF-TOKEN", "eyJpdiI6Im5CZnkzbGdDV2dCKzJtT0l6SXFXSEE9PSIsInZhbHVlIjoiTHlSZGFkZUVCMzNLdk5oT0xKOHA0RHNFNk9kNldFTUtqR2gxUytHVml6MEljcmY2aGNjK0czNTl1VVFha0JXTHJkdlJHbWptWkFCMjJsTTRzTFJPNFVHbXp4U0kxYlNEOW11Zi9yVmFkZUtXKzJtOTVtWi94VU1mY0YwNXpPN3kiLCJtYWMiOiI2NTMzZjEzYmY2MjI1OTlmYmJlYTA0Y2E1MGY5OTExYTRiMTMxYzk0MjJiMTcyZmE2N2Y5YTA3NmM0MjY0MDg1IiwidGFnIjoiIn0=".parse().unwrap());
     headers.insert("sec-ch-ua-mobile", "?0".parse().unwrap());
-    headers.insert("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36".parse().unwrap());
-    headers.insert("Accept", "application/json, text/plain, */*".parse().unwrap());
-    headers.insert("Referer", "https://www.oddsportal.com/football/england/premier-league/results/".parse().unwrap());
-    headers.insert("X-Requested-With", "XMLHttpRequest".parse().unwrap());
     headers.insert("sec-ch-ua-platform", "\"macOS\"".parse().unwrap());
+    headers.insert("sec-fetch-dest", "empty".parse().unwrap());
+    headers.insert("sec-fetch-mode", "cors".parse().unwrap());
+    headers.insert("sec-fetch-site", "same-origin".parse().unwrap());
+    headers.insert("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36".parse().unwrap());
+    headers.insert("x-requested-with", "XMLHttpRequest".parse().unwrap());
 
     let client = reqwest::Client::new();
-    let response: Response = client.get("https://www.oddsportal.com/ajax-all-events/topEvents")
+    let response = client.get("https://www.oddsportal.com/ajax-sport-country-tournament-archive_/1/tdkpynmB/X0/1/0/?_=1695458829998")
     .headers(headers)
     .send()
     .await?
-    .json()
+    .json::<Value>()
     .await?;
 
-    println!("{:?}", response.d.data);
+    // Access and work with the JSON data using serde_json::Value methods
+    let json = &response["d"]["rows"].as_array().unwrap();
+    let json_len: usize = json.len();
 
-    // Collect fields across structs in vector.
-    let names: Vec<String> = response.d.data.iter().map(|p| p.name.clone()).collect();
-    let urls: Vec<String> = response.d.data.iter().map(|p| p.url.clone()).collect();
-    let sport_ids: Vec<u32> = response.d.data.iter().map(|p| p.sport_id).collect();
-    let sport_urls: Vec<String> = response.d.data.iter().map(|p| p.sport_url.clone()).collect();
-    let tournament_ids: Vec<u32> = response.d.data.iter().map(|p| p.tournament_id).collect();
+    let mut all_fields_res: Vec<Series> = vec![];
+    let all_fields: [&str; 2] = ["home-name", "away-name"];
+    for f in all_fields {
+        let mut fields: Vec<String> = vec![];
+        for i in 0..json_len {
+            let json_val = get_actual_value(json[i][f].clone()).unwrap();
+            let mut json_val = vec![json_val];
+            fields.append(&mut json_val);
+        }
+        let mut fields: Vec<Series> = vec![Series::new(f, fields)];
+        all_fields_res.append(&mut fields);
+    }
 
-    // Convert fields into Polars' series.
-    let names_series = Series::new("names", names);
-    let urls_series = Series::new("urls", urls);
-    let sport_ids_series = Series::new("sport_ids", sport_ids);
-    let sport_urls_series = Series::new("sport_urls", sport_urls);
-    let tournament_ids_series = Series::new("tournament_ids", tournament_ids);
-
-    // Make dataframe from pl series.
-    let df = DataFrame::new(vec![names_series, urls_series, sport_ids_series, sport_urls_series, tournament_ids_series]);
+    let df = DataFrame::new(all_fields_res).unwrap();
 
     println!("{:?}", df);
 
